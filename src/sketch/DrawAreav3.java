fc017c48258ac98ef5de7708a9f9d940eb7f4181
urekMazino <hoshikagikisame@gmail.com>
2016-08-25 04:32:33 -0600

    Implementing custom drawLine method with Bresenham Algorithm.

--- src/sketch/DrawAreav3.java
+++ src/sketch/DrawAreav3.java
@@ -22,6 +22,7 @@ import java.awt.event.MouseWheelListener;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
+import java.awt.image.DataBufferInt;
 import java.awt.image.WritableRaster;
 
 import javax.swing.JLabel;
@@ -32,17 +33,20 @@ import javax.swing.Scrollable;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
+import com.sun.javafx.geom.Line2D;
+
 import docks.SwingCanvas;
 import javafx.embed.swing.SwingNode;
 import javafx.scene.control.TabPane;
 import jdk.nashorn.internal.runtime.regexp.joni.MatcherFactory;
 import misc.ColorsUI;
+import sun.java2d.SunGraphics2D;
 
 public class DrawAreav3 extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 	// Image in which we're going to draw
-	  private BufferedImage image;
+	  private BufferedImage image,previewLayer;
 	  private TexturePaint textureBackground;
 	  // Graphics2D object ==> used to draw on
 	  private Graphics2D g2;
@@ -71,6 +75,7 @@ public class DrawAreav3 extends JPanel{
 	  
 	  private SwingCanvas SWC;
 	  
+	  private boolean endlessPencil = true;
 	  // Mouse coordinates
 	  public DrawAreav3(int w,int h,ToolManager TM,SwingCanvas SWC) {
 
@@ -185,7 +190,8 @@ public class DrawAreav3 extends JPanel{
 	        if (g2 != null) {
 	          // draw line if g2 context not null
 	          g2.setPaint(ColorsUI.transToAwtColor(TM.getForegroundColorProperty().get()));
-	          g2.drawLine(SP.x,SP.y,EP.x,EP.y);
+	          bresenhamDrawLine(g2,SP.x,SP.y,EP.x,EP.y);
+	          //g2.drawLine(SP.x,SP.y,EP.x,EP.y);
 	          // refresh draw area to repaint
 	          repaint();
 	        }
@@ -230,15 +236,16 @@ public class DrawAreav3 extends JPanel{
 	  
 	  protected void paintComponent(Graphics g) {
 		  if (image==null){
-			  System.out.println("woops");
 			  initGraphics();
 		  }
+		
+
 	    g.setColor(ColorsUI.getPalette().darkPasiveColor);
 	    g.fillRect(0, 0, this.getWidth(), this.getHeight());
 	    paintAlphaBackground((Graphics2D)g);
 	    g.drawImage(image, paddingx-vp.x, paddingy-vp.y,currentWidth,currentHeight, null);
 	   
-	    paintGrid(g,10,10,Color.BLACK);
+	    paintGrid(g,10,10,ColorsUI.getPalette().darkPasiveColor);
 	    paintPixelGrid(g);
 
 	    lastWidth = currentWidth;
@@ -247,7 +254,116 @@ public class DrawAreav3 extends JPanel{
 	    zooming = false;
 	  }
 	  
+	  private void bresenhamDrawLine(Graphics g,int x1, int y1,int x2,int y2){
+  		 int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+  		 if (((x2<0 || x2>=originalWidth) && (x1<0 || x1>=originalWidth)) || ((y2<0 || y2>=originalHeight) && (y1<0 || y1>=originalHeight))){
+  			 return;
+  		 } else {
+
+  		 }
+  		 x2 = Math.max(0, Math.min(x2, originalWidth-1));
+		 x1 = Math.max(0, Math.min(x1, originalWidth-1));
+		 y2 = Math.max(0, Math.min(y2, originalHeight-1));
+		 y1 = Math.max(0, Math.min(y1, originalHeight-1));
+  			
+  		/* boolean callAgainMirror = false;
+  		 int mirrorx1=x1,mirrorx2=x2,mirrory1=y1,mirrory2=y2;
+  		
+		if ((x2<0 || x2>=originalWidth) && (x1>=0 && x1<originalWidth)){
+			mirrorx2 = x2%originalWidth;
+			mirrorx1 = (x2<0)?originalWidth-1:0;
+			x2 = Math.max(0, Math.min(x2, originalWidth-1));
+			callAgainMirror = true;
+		} else if ((x1<0 || x1>=originalWidth) && (x2>=0 && x2<originalWidth)){
+			mirrorx1 = x1%originalWidth;
+			mirrorx2 = (x1<0)?originalWidth-1:0;
+			x1 = Math.max(0, Math.min(x1, originalWidth-1));
+			callAgainMirror = true;
+		} else {
+			if (endlessPencil){
+  		  		x2 = x2%originalWidth;
+  		   		x1 = x1%originalWidth;
+			} else {
+				return;
+			}
+		}
+		
+		if ((y2<0 || y2>=originalHeight) && (y1>=0 && y1<originalHeight)){
+			mirrory2 = y2%originalHeight;
+			mirrory1 = (y2<0)?originalHeight-1:0;
+			y2 = Math.max(0, Math.min(y2, originalHeight-1));
+			callAgainMirror = true;
+		} else if ((y1<0 || y1>=originalHeight) && (y2>=0 && y2<originalHeight)){
+			mirrory1 = y1%originalHeight;
+			mirrory2 = (y1<0)?originalHeight-1:0;
+			y1 = Math.max(0, Math.min(y1, originalHeight-1));
+			callAgainMirror = true;
+		} else {
+			if (endlessPencil){
+  		  		y2 = y2%originalHeight;
+  		   		y1 = y1%originalHeight;
+			} else {
+				return;
+			}
+		} 
+		
+		if (callAgainMirror){
+			bresenhamDrawLine(g,mirrorx1,mirrory1,mirrorx2,mirrory2);
+		}
+		*/
+		 
+  	    int Dx = x2 - x1;
+  	    int Dy = y2 - y1;
+
+  	    //# Increments
+  	    int Sx = Integer.signum(Dx); 
+  	    int Sy = Integer.signum(Dy);
+
+  	    //# Segment length
+  	    Dx = Math.abs(Dx); 
+  	    Dy = Math.abs(Dy); 
+  	    int D = Math.max(Dx, Dy);
+
+  	    //# Initial remainder
+  	    double R = D / 2;
+
+  	    int X = x1;
+  	    int Y = y1;
+  	    if(Dx > Dy)
+  	    {   
+  	        //# Main loop
+  	        for(int I=0; I<=D; I++)
+  	        {   
+  	  			 pixels[(Y*originalWidth)+X]=-16777216;
+  	            //# Update (X, Y) and R
+  	            X+= Sx; R+= Dy; //# Lateral move
+  	            if (R >= Dx)
+  	            {
+  	                Y+= Sy; 
+  	                R-= Dx; //# Diagonal move
+  	            }
+  	        }
+  	    }
+  	    else
+  	    {   
+  	        //# Main loop
+  	        for(int I=0; I<=D; I++)
+  	        {    
+  	        	pixels[(Y*originalWidth)+X]=-16777216;
+  	            //# Update (X, Y) and R
+  	            Y+= Sy; 
+  	            R+= Dx; //# Lateral move
+  	            if(R >= Dy)
+  	            {    
+  	                X+= Sx; 
+  	                R-= Dy; //# Diagonal move
+  	            }
+  	        }
+  	    }
+	  }
 	  
+
 	  public void paintAlphaBackground(Graphics2D g){
 		  g.setPaint(textureBackground);
 		  g.fillRect(paddingx-vp.x, paddingy-vp.y,currentWidth,currentHeight);
@@ -297,7 +413,47 @@ public class DrawAreav3 extends JPanel{
 
 		vp.setLocation(x,y);
 	}
-	
+	/*private void fillBucket(){
+		byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+	    int width = image.getWidth();
+	    int height = image.getHeight();
+	    boolean hasAlphaChannel = image.getAlphaRaster() != null;
+
+	      int[][] result = new int[height][width];
+	      if (hasAlphaChannel) {
+	         final int pixelLength = 4;
+	         for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+	            int argb = 0;
+	            argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
+	            argb += ((int) pixels[pixel + 1] & 0xff); // blue
+	            argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
+	            argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
+	            result[row][col] = argb;
+	            col++;
+	            if (col == width) {
+	               col = 0;
+	               row++;
+	            }
+	         }
+	      } else {
+	         final int pixelLength = 3;
+	         for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+	            int argb = 0;
+	            argb += -16777216; // 255 alpha
+	            argb += ((int) pixels[pixel] & 0xff); // blue
+	            argb += (((int) pixels[pixel + 1] & 0xff) << 8); // green
+	            argb += (((int) pixels[pixel + 2] & 0xff) << 16); // red
+	            result[row][col] = argb;
+	            col++;
+	            if (col == width) {
+	               col = 0;
+	               row++;
+	            }
+	         }
+	      }
+
+	      return result;
+	}*/
 	private void updateSize(){		
 		vp = new Rectangle(vp.getLocation(),getSize());
 		if (currentWidth>vp.getWidth() || currentHeight>vp.getHeight()){
@@ -314,9 +470,11 @@ public class DrawAreav3 extends JPanel{
 			  setVP(0,0);
 		  }
 	}
-	
+	private void CreateAction(){
+		
+	}
 	private void backgroundTextureInit(){
-		BufferedImage textureBackgroundImg = new BufferedImage(alphaBlockSize*2, alphaBlockSize*2,BufferedImage.TYPE_INT_RGB);
+		BufferedImage textureBackgroundImg = new BufferedImage(alphaBlockSize*2, alphaBlockSize*2,BufferedImage.TYPE_INT_ARGB);
 		Graphics2D currentContext = textureBackgroundImg.createGraphics();
 		currentContext.setPaint(ColorsUI.AlphaDark);
 		currentContext.fillRect(0, 0, alphaBlockSize, alphaBlockSize);
@@ -332,12 +490,11 @@ public class DrawAreav3 extends JPanel{
       image = new BufferedImage(originalWidth, originalHeight,BufferedImage.TYPE_INT_ARGB);
       g2 = (Graphics2D) image.getGraphics();
       g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-  
 	}
 	
 	private void initGraphics(){
-		initImage();
 		backgroundTextureInit();
+		initImage();
 	}
 	
 }
